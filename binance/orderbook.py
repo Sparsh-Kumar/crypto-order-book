021260849282450e458aff977cc8f880e894cec0
Sparsh Kumar <sparshkumar14998@gmail.com>
2025-04-01 20:20:32 +0530

    Latency calculation added.

--- binance/orderbook.py
+++ binance/orderbook.py
@@ -2,6 +2,8 @@ import json
 import os
 import websocket
 import requests
+from datetime import datetime, timezone
+import time
 
 config = {}
 with open("config.json", "r") as file:
@@ -12,22 +14,31 @@ REST_API_ENDPOINT = config['FUTURES']['REST_API_ENDPOINT']
 
 class BinanceWebSocketClient:
 
-  def __init__(self, url = '', ticker='btcusdt', orderBook = None):
+  def __init__(self, url = '', ticker='btcusdt', orderBook = None, checkLatency = False, checkLatencyRecords = 100):
     self.ws = None
     self.ticker = ticker
     self.url = f'{url}?streams={self.ticker}@depth@100ms'
     self.orderBook = orderBook
+    self.totalLatency = 0
+    self.checkLatency = checkLatency
+    self.checkLatencyRecords = checkLatencyRecords
+    self.checkLatencyRecordsCopy = checkLatencyRecords
 
-  def connect(self):
+  def onConnect(self):
+
+    onMessageFunc = self.onMessage
+    if (self.checkLatency):
+      onMessageFunc = self.onCheckLatency
+
     self.ws = websocket.WebSocketApp(
       self.url,
-      on_open=self.on_open,
-      on_message=self.on_message,
-      on_close=self.on_close
+      on_open=self.onOpen,
+      on_message=onMessageFunc,
+      on_close=self.onClose
     )
     self.ws.run_forever()
 
-  def on_open(self, ws):
+  def onOpen(self, ws):
     subscribe_message = {
       "method": "SUBSCRIBE",
       "params": [f"{self.ticker}@depth"],
@@ -35,12 +46,33 @@ class BinanceWebSocketClient:
     }
     ws.send(json.dumps(subscribe_message))
 
-  def on_message(self, ws, message):
+  def onMessage(self, ws, message):
     data = json.loads(message)
     self.orderBook.create(data)
 
-  def on_close(self, ws):
+  def onCheckLatency(self, ws, message):
+    data = json.loads(message)
+
+    if (not data['data']['E']):
+      return
+
+    if (not self.checkLatencyRecords):
+      self.onClose(ws)
+
+    eventTime = datetime.fromtimestamp(data['data']['E'] / 1000, tz=timezone.utc)
+    currentTime = datetime.now(timezone.utc)
+    timeDiffInMs = int((currentTime - eventTime).total_seconds() * 1000)
+    self.totalLatency += timeDiffInMs
+    self.checkLatencyRecords -= 1
+    averageLatency = self.totalLatency / (self.checkLatencyRecordsCopy - self.checkLatencyRecords)
+    print(f'Total Latency = {self.totalLatency}, Records Remaining = {self.checkLatencyRecords}, Records Checked = {self.checkLatencyRecordsCopy - self.checkLatencyRecords}, Average Latency = {averageLatency}')
+
+  def onClose(self, ws):
     print("Connection closed")
+    ws.close()
+
+  def getAverageLatency(self):
+    return self.totalLatency / self.checkLatencyRecordsCopy
 
 
 class OrderBook:
@@ -108,11 +140,12 @@ class BinanceWebSocketService:
     self.client = client
 
   def start_stream(self):
-    self.client.connect()
+    self.client.onConnect()
 
 
 if __name__ == "__main__":
   orderBook = OrderBook(REST_API_ENDPOINT, 'btcusdt')
-  client = BinanceWebSocketClient(WEBSOCKET_STREAM_ENDPOINT, 'btcusdt', orderBook)
+  client = BinanceWebSocketClient(WEBSOCKET_STREAM_ENDPOINT, 'btcusdt', orderBook, False)
   service = BinanceWebSocketService(client)
   service.start_stream()
+  print(client.getAverageLatency())
